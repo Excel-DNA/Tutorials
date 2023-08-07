0e9e8a5616ca3ee391cb774104314a7ac152155f
sagi <icicles_truth.0f@icloud.com>
2023-08-07 21:50:02 +0800

    Update to SDK-style

--- Fundamentals/ArgumentTypeBasics/My Project/AssemblyInfo.vb
+++ Fundamentals/ArgumentTypeBasics/My Project/AssemblyInfo.vb
@@ -8,10 +8,10 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("ArgumentTypeBasics")>
+'<Assembly: AssemblyTitle("ArgumentTypeBasics")>
 <Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("ArgumentTypeBasics")>
+'<Assembly: AssemblyCompany("")>
+'<Assembly: AssemblyProduct("ArgumentTypeBasics")>
 <Assembly: AssemblyCopyright("Copyright ©  2020")>
 <Assembly: AssemblyTrademark("")>
 
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+'<Assembly: AssemblyVersion("1.0.0.0")>
+'<Assembly: AssemblyFileVersion("1.0.0.0")>
