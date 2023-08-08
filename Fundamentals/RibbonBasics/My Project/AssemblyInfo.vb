84a7a514b04fbf1a8b3521247a31186bd9b70686
sagi <icicles_truth.0f@icloud.com>
2023-08-08 22:33:58 +0800

    Update to SDK-style

--- Fundamentals/RibbonBasics/My Project/AssemblyInfo.vb
+++ Fundamentals/RibbonBasics/My Project/AssemblyInfo.vb
@@ -8,10 +8,10 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("RibbonBasics")>
+'<Assembly: AssemblyTitle("RibbonBasics")>
 <Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("RibbonBasics")>
+'<Assembly: AssemblyCompany("")>
+'<Assembly: AssemblyProduct("RibbonBasics")>
 <Assembly: AssemblyCopyright("Copyright ©  2020")>
 <Assembly: AssemblyTrademark("")>
 
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+'<Assembly: AssemblyVersion("1.0.0.0")>
+'<Assembly: AssemblyFileVersion("1.0.0.0")>
--- Fundamentals/RibbonBasics/Ribbon.vb
+++ Fundamentals/RibbonBasics/Ribbon.vb
@@ -2,18 +2,26 @@
 Imports Microsoft.Office.Interop.Excel
 Imports ExcelDna.Integration.CustomUI
 Imports ExcelDna.Integration
+Imports RibbonBasics.My.Resources
 
 <ComVisible(True)>
 Public Class Ribbon
     Inherits ExcelRibbon
 
+    Public Overrides Function GetCustomUI(RibbonID As String) As String
+        Return RibbonResources.Ribbon ' The name here is the resource name that the ribbon xml has in the RibbonResources resource file
+    End Function
+
+    Public Overrides Function LoadImage(imageId As String) As Object
+        ' This will return the image resource with the name specified in the image='xxxx' tag
+        Return RibbonResources.ResourceManager.GetObject(imageId)
+    End Function
     Public Sub OnSayHelloPressed(control As IRibbonControl)
         Dim app As Application
         Dim rng As Range
 
         app = ExcelDnaUtil.Application
         rng = app.Range("A1")
-
         rng.Value = "Hello from .NET!"
 
     End Sub
